1ba3ca092f6aecc18af2a9facb2cb70a821c1a4a
Sultonov-R <sultonovrozali@gmail.com>
2024-04-25 14:18:47 +0500

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import Pagination from "@mui/material/Pagination";
-import "./App.css";
 import { useEffect, useState } from "react";
+import "./App.css";
 import Car from "./components/Car";
 
 interface CarType {
@@ -20,71 +19,61 @@ interface ResponseProps {
     limit: number;
   };
   results: CarType[];
-  total: number;
 }
 
 function App() {
   const [cars, setCars] = useState<CarType[]>([]);
+  const [fetching, setFetching] = useState<boolean>(true);
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [limit, setLimit] = useState<number>(8);
-  const [total, setTotal] = useState<number>(10);
 
-  async function getData(limit: number, page: number) {
-    try {
-      const resp = await fetch(
-        `http://localhost:3000/machines?limit=${limit}&&page=${page}`
-      );
-      const data: ResponseProps = await resp.json();
-      setCars(data.results);
-      setTotal(data.total);
-    } catch (error) {
-      console.log(error);
+  async function getData(limit: number = 5, page: number) {
+    const resp = await fetch(
+      `http://localhost:3000/machines?limit=${limit}&&page=${page}`
+    );
+    const data: ResponseProps = await resp.json();
+    setCars([...cars, ...data.results]);
+    setFetching(false);
+  }
+
+  useEffect(() => {
+    if (fetching) {
+      getData(10, currentPage);
+    }
+  }, [fetching, currentPage]);
+
+  useEffect(() => {
+    document.addEventListener("scroll", scrollHandler);
+
+    return function () {
+      document.removeEventListener("scroll", scrollHandler);
+    };
+  }, []);
+
+  function scrollHandler(e: Event) {
+    const target = e.target as HTMLDocument;
+    if (
+      target.documentElement.scrollHeight -
+        (target.documentElement.scrollTop + window.innerHeight) <
+      100
+    ) {
+      setFetching(true);
+      setCurrentPage((currentPage) => currentPage + 1);
     }
   }
 
-  useEffect(() => {
-    getData(limit, currentPage);
-  }, []);
-
-  useEffect(() => {
-    getData(limit, currentPage);
-  }, [limit, currentPage]);
-
-  function handleChange(e: React.ChangeEvent<unknown>, count: number) {
-    setCurrentPage(count);
-  }
-
   return (
-    <div className="wrapper">
-      <div className="cars_container">
-        {cars.length &&
-          cars.map((car) => {
-            return (
-              <Car
-                image={car.image}
-                title={car.title}
-                start_production={car.start_production}
-                class={car.class}
-              />
-            );
-          })}
-      </div>
-      <div className="footer">
-        <Pagination
-          onChange={handleChange}
-          count={Math.trunc(total / limit)}
-          variant="outlined"
-          shape="rounded"
-        />
-        <select
-          value={limit}
-          onChange={(e) => setLimit(Number(e.target.value))}
-        >
-          <option value={8}>8</option>
-          <option value={12}>12</option>
-          <option value={16}>16</option>
-        </select>
-      </div>
+    <div className="container">
+      {cars.length > 0 &&
+        cars.map((car) => {
+          return (
+            <Car
+              image={car.image}
+              title={car.title}
+              start_production={car.start_production}
+              class={car.class}
+            ></Car>
+          );
+        })}
     </div>
   );
 }
